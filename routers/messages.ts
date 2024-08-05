107746830c9b842c0ac627ac55ab8ba2cae2e0ba
Gordus Alexander <aldrmiller@gmail.com>
2024-08-06 00:45:52 +0600

    реализовал функцию получения сообщения через get. обработал ошибки через try catch.

--- routers/messages.ts
+++ routers/messages.ts
@@ -5,6 +5,27 @@ import {MessageData} from "../types";
 const messagesRouter = express.Router();
 const fileMessagePath = './messages';
 
+messagesRouter.get('/', async (_, res) => {
+    try {
+        const messageDataArray: MessageData[] = [];
+        const files = await fs.readdir(fileMessagePath);
+
+        if (files.length !== 0) {
+            for (const file of files) {
+                const fileContents = await fs.readFile(`./messages/${file}`);
+                const result: MessageData = JSON.parse(fileContents.toString());
+                messageDataArray.push(result);
+            }
+            return res.status(200).send(messageDataArray.slice(-5));
+        } else {
+            return res.status(400).send([]);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('An error occurred while reading data');
+    }
+});
+
 messagesRouter.post('/', async (req, res) => {
     try {
         const postData: MessageData = {
@@ -12,13 +33,17 @@ messagesRouter.post('/', async (req, res) => {
             datetime: new Date().toISOString(),
         };
 
-        const filePatch = `${fileMessagePath}/${postData.datetime}.txt`;
-        await fs.writeFile(filePatch, JSON.stringify(postData));
-        return res.status(200).send(postData);
+        if (Object.values(postData).every(value => value.trim().length > 0)) {
+            const filePatch = `${fileMessagePath}/${postData.datetime}.txt`;
+            await fs.writeFile(filePatch, JSON.stringify(postData));
+            return res.status(200).send(postData);
+        } else {
+            return res.status(400).send('Invalid input');
+        }
 
     } catch (error) {
         console.error(error);
-        return res.status(500);
+        return res.status(500).send('An error occurred while sending the request');
     }
 });
 
